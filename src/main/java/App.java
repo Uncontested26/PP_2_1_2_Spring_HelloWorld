c0d12d6c26c9eee79e62b970d2e057915c82fabd
MR.FIRST <128628624+Uncontested26@users.noreply.github.com>
2023-05-09 21:54:26 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class A {
+public class App{
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
